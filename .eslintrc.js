dc688a5c4592f2fb444c9d7a5e4069780d4f9095
Alf <alf.g.jr@gmail.com>
2019-10-07 20:21:58 -0700

    Update package-lock. Fix eslintrc for airbnb and prettier. Add .prettierignore.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/prettier'],
+  extends: ['plugin:vue/essential', '@vue/eslint-config-airbnb', '@vue/eslint-config-prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
