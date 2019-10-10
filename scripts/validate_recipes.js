6a7484ad88b17853dffa764e526d5ff1c1b2bce1
Tobias Christiansen <christiansen@tkn-consulting.de>
2019-10-10 10:54:35 +0200

    Resolved prettiert warnings.

--- scripts/validate_recipes.js
+++ scripts/validate_recipes.js
@@ -12,19 +12,18 @@ const findRecipeFiles = (err, files) => {
 
 const validateRecipe = files => {
   files.forEach(file => {
-    var contents = fs.readFileSync(__dirname + "/../src/recipes/" + file, 'utf8');
+    var contents = fs.readFileSync(__dirname + '/../src/recipes/' + file, 'utf8');
     var jsonContents = JSON.parse(contents);
 
     var valid = ajv.validate('recipeSchema', jsonContents);
     if (!valid) {
       console.log(`Validation failed for ${file}!`);
-      console.log(ajv.errorsText())
+      console.log(ajv.errorsText());
     }
-
   });
 };
 
 const schema = fs.readFileSync('scripts/recipe_schema.json', 'utf-8');
 const schemaJson = JSON.parse(schema);
 ajv.addSchema(schemaJson, 'recipeSchema');
-fs.readdir(__dirname + "/../src/recipes", findRecipeFiles);
+fs.readdir(__dirname + '/../src/recipes', findRecipeFiles);
