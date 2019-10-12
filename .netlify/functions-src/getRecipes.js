fca936e154283d71585fe559cc23210aa922f2b6
Alf <alf.g.jr@gmail.com>
2019-10-12 01:23:13 -0700

    fixing more conflicts

--- .netlify/functions-src/getRecipes.js
+++ .netlify/functions-src/getRecipes.js
@@ -7,10 +7,7 @@ exports.handler = function(event, context, callback) {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
-<<<<<<< HEAD
-=======
       'Cache-Control': 'public, s-maxage=3600' // 60 minutes.
->>>>>>> 855fe49fcd477323400d7680f0b4117ee7112ba7
     },
     body: JSON.stringify({ recipes: r }),
   });
