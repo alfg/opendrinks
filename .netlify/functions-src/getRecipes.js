77ca4344d8feca421ffc1cae90beebcbfa934b90
Alf <alf.g.jr@gmail.com>
2019-10-11 23:23:44 -0700

    update getRecipes lambda function.

--- .netlify/functions-src/getRecipes.js
+++ .netlify/functions-src/getRecipes.js
@@ -1,13 +1,13 @@
-import { getRecipes } from '../../src/recipes/index';
+import recipes from '../../src/recipes/index';
 
 exports.handler = function(event, context, callback) {
-  // const r = recipes.getRecipes();
+  const r = recipes.getRecipes();
 
   return callback(null, {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
-    // body: JSON.stringify({ recipes: r }),
+    body: JSON.stringify({ recipes: r }),
   });
 }
