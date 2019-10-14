0131234958e902b7665c069dbb7b885064c6d5c5
Alf <alf.g.jr@gmail.com>
2019-10-13 18:43:22 -0700

    cleanup on netlify getRecipe function.

--- .netlify/functions-src/getRecipe.js
+++ .netlify/functions-src/getRecipe.js
@@ -5,13 +5,6 @@ exports.handler = function(event, context, callback) {
   const id = path.split('/').slice(-1)[0];
   const r = recipes.getRecipe(id);
 
-  // const img = require(`../../src/assets/recipes/${r.image}`);
-  // console.log(img);
-
-  // const img = require(`../../src/assets/recipes/${r.image}`);
-  // console.log('img: ', img);
-  // r.img = img;
-
   return callback(null, {
     statusCode: 200,
     headers: {
