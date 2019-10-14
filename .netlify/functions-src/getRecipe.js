3181072c040083fbfb081f8218b5a560dabde9d3
Alf <alf.g.jr@gmail.com>
2019-10-13 23:41:32 -0700

    trycatch on getRecipe function.

--- .netlify/functions-src/getRecipe.js
+++ .netlify/functions-src/getRecipe.js
@@ -3,7 +3,19 @@ import recipes from '../../src/recipes/index';
 exports.handler = function(event, context, callback) {
   const path = event.path || null;
   const id = path.split('/').slice(-1)[0];
-  const r = recipes.getRecipe(id);
+  let r;
+
+  try {
+    r = recipes.getRecipe(id);
+  } catch (error) {
+    return callback(null, {
+      statusCode: 404,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ code: 404, message: 'Recipe not found.' }),
+    });
+  }
 
   return callback(null, {
     statusCode: 200,
