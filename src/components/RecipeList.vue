ce6843060a0fdecc78b656bc18b8063b60e0519d
Alf <alf.g.jr@gmail.com>
2019-10-14 22:49:29 -0700

    more formatting for recipe explore page.

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,13 +1,12 @@
 <template>
-  <div id="recipe-list" class="container">
+  <div id="recipe-list" class="">
     <b-card-group deck>
-      <div v-for="(o, i) in paginatedItems" v-bind:key="i" class="mb-2 p-1">
+      <div v-for="(o, i) in paginatedItems" v-bind:key="i" class="card-wrapper mb-2">
         <b-card
           :title="o.name"
           :img-src="o.image ? require(`@/assets/recipes/${o.image}`) : null"
           :img-alt="o.name"
           img-top
-          style="max-width: 20rem;"
         >
           <b-card-text>
             {{ o.description }}
@@ -23,22 +22,20 @@
       </div>
     </b-card-group>
 
-    <b-container>
-      <b-row>
-        <b-col cols="12" md="10">
-          <b-pagination
-            @change="onPageChanged"
-            :total-rows="rows"
-            :per-page="selected"
-            v-model="currentPage"
-          ></b-pagination>
-        </b-col>
-        <b-col cols="12" md="2">
-          <b-form-select v-model="selected" :options="options" v-on:change="getSelectedItem">
-          </b-form-select>
-        </b-col>
-      </b-row>
-    </b-container>
+    <b-row class="mt-4">
+      <b-col cols="12" md="10">
+        <b-pagination
+          @change="onPageChanged"
+          :total-rows="rows"
+          :per-page="selected"
+          v-model="currentPage"
+        ></b-pagination>
+      </b-col>
+      <b-col cols="12" md="2">
+        <b-form-select v-model="selected" :options="options" v-on:change="getSelectedItem">
+        </b-form-select>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -57,10 +54,10 @@ export default {
   data() {
     return {
       currentPage: 1,
-      perPage: 10,
-      selected: 10,
+      perPage: 12,
+      selected: 12,
       pageNumber: 0,
-      options: [{ value: 10, text: '10' }, { value: 25, text: '25' }, { value: 50, text: '50' }],
+      options: [{ value: 12, text: '12' }, { value: 24, text: '24' }, { value: 48, text: '48' }],
       favorites: [],
     };
   },
@@ -84,6 +81,7 @@ export default {
   methods: {
     onPageChanged(page) {
       this.pageNumber = page - 1;
+      window.scrollTo(0, 0);
     },
     getSelectedItem(event) {
       this.perPage = event;
@@ -103,6 +101,16 @@ export default {
 </script>
 
 <style scoped>
+.card-wrapper {
+  width: 315px;
+}
+
+@media (max-width: 768px) {
+  .card-wrapper {
+    width: 100%;
+  }
+}
+
 .card-deck .card {
   margin-right: 0;
 }
