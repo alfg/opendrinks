a1eb8a6647bb5bde0e4bb03df6b7ded2ea1ed3a8
Josef Biehler <gabbersepp@googlemail.com>
2019-10-16 07:45:11 +0200

    add test for view 'Random'

--- tests/unit/views/Random.spec.js
+++ tests/unit/views/Random.spec.js
@@ -1,19 +1,17 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 import BootstrapVue from 'bootstrap-vue';
-
 import Random from '@/views/Random.vue';
 
-
-
 describe('Random view', () => {
-    let fn = jest.fn();
+  let fn;
+  let wrapper;
 
-
-
-  test('should display anything', () => {
+  beforeEach(() => {
     const localVue = createLocalVue();
-localVue.use(BootstrapVue);
-    const wrapper = mount(Random, {
+    localVue.use(BootstrapVue);
+    fn = jest.fn();
+
+    wrapper = mount(Random, {
       localVue,
       mocks: {
         $router: {
@@ -21,7 +19,18 @@ localVue.use(BootstrapVue);
         }
       }
     });
+  });
+  
+  test('should route to random recipe', () => {
     expect(wrapper.vm.$router.replace).toHaveBeenCalled();
   });
 
+  test('should route to another recipe after clicking on "next" button', () => {
+    wrapper.find('.float-right > *').trigger('click');
+    expect(wrapper.vm.$router.replace).toHaveBeenCalledTimes(2);
+    const call1Parameter = fn.mock.calls[0];
+    const call2Parameter = fn.mock.calls[1];
+    // both calls must lead to different recipes
+    expect(call1Parameter).not.toBe(call2Parameter);
+  });
 });
