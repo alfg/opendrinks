f3e5f51eacbf15a36e03af8ebe79d56a35166fed
Alf <alf.g.jr@gmail.com>
2019-10-16 22:56:29 -0700

    Add 'load more' feature for recipes on the featured list.

--- src/components/FeaturedRecipes.vue
+++ src/components/FeaturedRecipes.vue
@@ -5,11 +5,20 @@
       <p>{{ feature.description }}</p>
       <b-card-group deck>
         <RecipeTile
-          v-for="(item, j) in feature.items.slice(0, featuredLimit)"
+          v-for="(item, j) in feature.items.slice(0, feature.size)"
           v-bind:key="j"
           v-bind:id="item"
         />
       </b-card-group>
+      <b-button
+        block
+        variant="outline-primary"
+        v-if="feature.items.length > featuredItemSize && feature.size < feature.items.length"
+        v-on:click="loadMore(i)"
+      >
+        Load More</b-button
+      >
+
       <hr v-if="featuredList.length !== i + 1" />
     </div>
   </div>
@@ -19,6 +28,8 @@
 import recipes from '../recipes';
 import RecipeTile from './RecipeTile.vue';
 
+const FEATURED_ITEM_SIZE = 6;
+
 export default {
   name: 'FeaturedRecipes',
   components: {
@@ -26,13 +37,18 @@ export default {
   },
   data() {
     return {
-      featuredLimit: 6,
       featuredList: [],
+      featuredItemSize: FEATURED_ITEM_SIZE,
     };
   },
   created() {
     window.document.title = `Open Drinks - Featured`;
     this.featuredList = recipes.getFeaturedRecipes();
+  },
+  methods: {
+    loadMore(i) {
+      this.featuredList[i].size += FEATURED_ITEM_SIZE;
+    },
   },
 };
 </script>
