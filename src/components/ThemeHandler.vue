2fd64832906380eacdcfee9cdc36449951ba163c
Fabrizio Tornatore <torfab94@gmail.com>
2020-10-03 04:02:32 +0200

    feat(ThemeHandler): Refactor logic behind theme Handling, added new slider
    now setTheme can be called from external sources and is selfcontained making easier to add new theme in future develop

--- src/components/ThemeHandler.vue
+++ src/components/ThemeHandler.vue
@@ -1,7 +1,8 @@
 <template>
-  <b-button @click="toggleTheme()" class="theme ml-5 text-capitalize"
-      >{{theme}} Theme
-  </b-button>
+  <div>
+    <input id="theme-toggle" v-model="darkCheckBox" @input="checkboxHandler" type="checkbox" />
+    <label for="theme-toggle"><span></span></label>
+  </div>
 </template>
 
 <script>
@@ -9,29 +10,37 @@ export default {
   data() {
     return {
       theme: localStorage.getItem('theme') || 'light',
+      darkCheckBox: false,
     };
   },
   mounted() {
     this.setTheme(this.theme);
+    this.darkCheckBox = this.theme === 'dark';
   },
   methods: {
+    checkboxHandler() {
+      if (this.darkCheckBox) {
+        this.setTheme('light');
+      } else {
+        this.setTheme('dark');
+      }
+    },
     setTheme(theme) {
-      document.documentElement.setAttribute('data-theme', theme)
+      localStorage.setItem('theme', theme);
+      document.documentElement.setAttribute('data-theme', theme);
     },
     toggleTheme() {
-      switch(this.theme){
+      switch (this.theme) {
         case 'dark':
           this.theme = 'light';
-          localStorage.setItem('theme', 'light');
           break;
         case 'light':
           this.theme = 'dark';
-          localStorage.setItem('theme', 'dark');
           break;
         default:
           break;
       }
-      this.setTheme(this.theme) 
+      this.setTheme(this.theme);
     },
   },
 };
