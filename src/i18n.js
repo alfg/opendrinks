8eba90b8ca576bd3d0d8249504ed9174b1c08d3e
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-10-02 21:12:34 +0900

    enable silent warnings

--- src/i18n.js
+++ src/i18n.js
@@ -6,4 +6,6 @@ Vue.use(VueI18n);
 export default new VueI18n({
   locale: (window.navigator.language && window.navigator.language.split('-')[0]) || 'en',
   fallbackLocale: 'en',
+  silentTranslationWarn: true,
+  silentFallbackWarn: true,
 });
