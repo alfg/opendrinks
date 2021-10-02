08efd10bba8a4f6d41a68cfe9d6ac25b46c36b67
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-10-02 21:04:56 +0900

    determine language from window object

--- src/i18n.js
+++ src/i18n.js
@@ -4,6 +4,6 @@ import VueI18n from 'vue-i18n';
 Vue.use(VueI18n);
 
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'ja',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: (window.navigator.language && window.navigator.language.split('-')[0]) || 'en',
+  fallbackLocale: 'en',
 });
