eb35f6985a43d3f704284e732bcc7b92e57f18fa
OscarRivasGonzalez <oscar.rivas@decathlon.com>
2021-10-13 23:13:13 +0200

    add es-translation for enter your keyword

--- src/views/KeywordList.vue
+++ src/views/KeywordList.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="container">
-    <b-form-input v-model="filter" placeholder="Enter your keyword"></b-form-input>
+    <b-form-input
+      v-model="filter"
+      :placeholder="$t('Enter your keyword')"
+    ></b-form-input>
 
     <b-list-group class="mt-4">
       <b-list-group-item
-        v-for="(keyword, index) in keywords.filter((row) => {return row.includes(filter) || !filter})"
+        v-for="(keyword, index) in keywords.filter(row => {
+          return row.includes(filter) || !filter;
+        })"
         :key="index"
         :to="{ name: 'keyword', params: { keyword: urlEncode(keyword) } }"
-        >{{ keyword }}</b-list-group-item
-      >
+      >{{ keyword }}</b-list-group-item>
     </b-list-group>
   </div>
 </template>
@@ -22,7 +26,7 @@ export default {
     const keywords = recipes.getAllKeywords().sort();
     return {
       keywords,
-      filter: null
+      filter: null,
     };
   },
   mounted() {
@@ -35,3 +39,17 @@ export default {
   },
 };
 </script>
+
+<i18n>
+{
+  "no": {
+    "Enter your keyword": "Søk etter nøkkelord"
+  },
+  "hi": {
+    "Enter your keyword": "अपना सूचक-शब्द दर्ज करें"
+  },
+  "es": {
+    "Enter your keyword": "Introduce tu palabra clave"
+  }
+}
+</i18n>
